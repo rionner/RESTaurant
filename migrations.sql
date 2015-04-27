ad5f1d7dfaa348860f5c328a02dc6c8e42f13def
Marion Huebner <mphuebner@gmail.com>
2015-04-27 12:08:02 -0500

    refactored database migrations

--- migrations.sql
+++ migrations.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE  restaurant;
 \c restaurant;
 
-CREATE TABLE parties (id SERIAL PRIMARY KEY, table_num INTEGER, num_people INTEGER, paid BOOLEAN);
+CREATE TABLE parties (id SERIAL PRIMARY KEY, table_num INTEGER);
 
-CREATE TABLE orders (id SERIAL PRIMARY KEY, parties_id INTEGER REFERENCES parties(id), total INTEGER);
+CREATE TABLE foods (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT, cents INTEGER, vegan BOOLEAN);
 
-CREATE TABLE foods (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT, price INTEGER, vegan BOOLEAN,  orders_id INTEGER);
+CREATE TABLE orders (id SERIAL PRIMARY KEY, parties_id INTEGER REFERENCES parties(id), foods_id INTEGER REFERENCES foods(id), is_paid BOOLEAN);
