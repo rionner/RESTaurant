cdf60c2f50a8df0c73e442408bcfb9fc74d70c44
Marion Huebner <mphuebner@gmail.com>
2015-04-27 15:20:41 -0500

    update gem list, database refactored

--- Gemfile
+++ Gemfile
@@ -2,5 +2,8 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-activerecord'
+gem 'bcrypt'
+gem 'rake'
+gem 'pry'
 gem 'pg'
 gem 'json'
--- migrations.sql
+++ migrations.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE  restaurant;
 \c restaurant;
 
-CREATE TABLE parties (id SERIAL PRIMARY KEY, table_num INTEGER);
+CREATE TABLE parties (id SERIAL PRIMARY KEY, table_num INTEGER, is_paid BOOLEAN);
 
 CREATE TABLE foods (id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT, cents INTEGER, vegan BOOLEAN);
 
-CREATE TABLE orders (id SERIAL PRIMARY KEY, parties_id INTEGER REFERENCES parties(id), foods_id INTEGER REFERENCES foods(id), is_paid BOOLEAN);
+CREATE TABLE orders (id SERIAL PRIMARY KEY, parties_id INTEGER, foods_id INTEGER);
+
+CREATE TABLE users ( id SERIAL PRIMARY KEY, username VARCHAR(255), password_hash VARCHAR(255));
